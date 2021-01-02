c7d7e492ebc0a627f149b612a049d5cec9545cc6
Tylryan <Darkstar1!>
2021-01-02 15:24:02 -0600

    It works

---
+++ Bot.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import random
+import csv
+
+content = []
+
+with open('purchase_record.csv','r') as file:
+    csv_reader = csv.reader(file, delimiter = ',')
+    for i in csv_reader:
+        content.append(i)
+print(content)
+stock_price = random.randint(50,100)
+stock_held = 0
+print(stock_price)
+
+stock_held = int(content[-1][2])
+if stock_price >= 70:
+    if stock_held == 0:
+        stock_held +=1
+        content.append([stock_price,'Bought',stock_held])
+        print('Bought')
+    else:
+        print('You already have stock')
+
+if stock_price <70:
+    if stock_held == 1:
+        stock_held -= 1
+        content.append([stock_price,'Sold', stock_held])
+        print('Sold')
+    else:
+        print('You do not have stock to sell')
+
+with open('purchase_record.csv','w', newline= '') as file:
+    csv_writer = csv.writer(file, delimiter = ',')
+    for i in content:
+        csv_writer.writerow(i)
